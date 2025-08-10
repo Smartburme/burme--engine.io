3e9c5913b5aa567e3ae319f5cfa044f6f65355d0
burmeai.io <athuyrain@gmail.com>
2025-08-10 15:49:37 +0630

    Create happy.ans.js
    Signed-off-by: burmeai.io <athuyrain@gmail.com>

---
+++ answer/happy.ans.js
@@ -0,0 +1,6 @@
+const happyAnswers = {
+  "I'm happy": "မင်္ဂလာပါနော်! ချမ်းသာမှုနဲ့ပျော်ရွှင်မှုတွေ ဆက်လက်ပါစေပါသည်။",
+  "Feeling great": "ကောင်းပါပြီ။ အဆင်ပြေပါစေ။"
+};
+
+export default happyAnswers;
