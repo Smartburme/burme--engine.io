7610d17fd498749f49268882d8f709f42e060ec4
burmeai.io <athuyrain@gmail.com>
2025-08-10 15:57:11 +0630

    Create package.js
    Signed-off-by: burmeai.io <athuyrain@gmail.com>

---
+++ package.js
@@ -0,0 +1,25 @@
+{
+  "name": "burme-engine",
+  "version": "1.0.0",
+  "description": "Burme Engine - Knowledge based AI chatbot project",
+  "main": "server.js",
+  "type": "module",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "keywords": [
+    "ai",
+    "chatbot",
+    "knowledge",
+    "burmese",
+    "burme-engine"
+  ],
+  "author": "Your Name",
+  "license": "MIT",
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=14"
+  }
+}
